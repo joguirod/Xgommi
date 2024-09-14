92d12f9a373d61701dfa4374c2e8b1880d9ee2e8
joguirod <joguicane@gmail.com>
2024-09-13 23:06:51 -0300

    first commit

---
+++ backend/XgommiApi/src/main/java/br/com/xgommiapi/XgommiApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.xgommiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XgommiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XgommiApiApplication.class, args);
+    }
+
+}
