eb3223b4e3839a98c7ba5682c809634d2a510004
joguirod <joguicane@gmail.com>
2024-09-14 01:25:14 -0300

    feat : add inital entities

---
+++ backend/XgommiApi/src/main/java/br/com/xgommiapi/domain/entity/GommiUser.java
@@ -0,0 +1,79 @@
+package br.com.xgommiapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class GommiUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idGommiUser;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "follower")
+    private List<GommiUser> following;
+
+    @OneToMany(mappedBy = "followed")
+    private List<GommiUser> followers;
+
+    @ManyToOne
+    private Community comunity;
+
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "userNotified")
+    private List<Notification> unreadNotifications;
+
+    private String biography;
+    private LocalDateTime registrationDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GommiUser gommiUser = (GommiUser) o;
+        return Objects.equals(idGommiUser, gommiUser.idGommiUser) && Objects.equals(login, gommiUser.login) && Objects.equals(email, gommiUser.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idGommiUser, login, email);
+    }
+
+    @Override
+    public String toString() {
+        return "GommiUser{" +
+                "idGommiUser=" + idGommiUser +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", biography='" + biography + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", comunity=" + comunity +
+                ", posts=" + posts +
+                ", unreadNotifications=" + unreadNotifications +
+                '}';
+    }
+}
---
+++ backend/XgommiApi/src/main/java/br/com/xgommiapi/domain/entity/Post.java
@@ -0,0 +1,62 @@
+package br.com.xgommiapi.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idPost;
+
+    @Column(nullable = false)
+    private String text;
+
+    @ManyToOne
+    private GommiUser author;
+
+    @OneToMany(mappedBy = "originalPost")
+    private List<Comment> commentsList;
+
+    @ManyToOne
+    private Community comunity;
+
+    private int upvotes;
+    private int downvotes;
+    private boolean isRepost;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(idPost, post.idPost) && Objects.equals(author, post.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idPost, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "idPost=" + idPost +
+                ", text='" + text + '\'' +
+                ", author=" + author +
+                ", commentsList=" + commentsList +
+                ", comunity=" + comunity +
+                ", upvotes=" + upvotes +
+                ", downvotes=" + downvotes +
+                ", isRepost=" + isRepost +
+                '}';
+    }
+}
