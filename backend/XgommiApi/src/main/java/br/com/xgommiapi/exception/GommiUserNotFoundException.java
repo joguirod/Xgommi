197d7fd52b5f78fa5e4c509aa114824ab5289e73
joguirod <joguicane@gmail.com>
2024-09-14 03:11:26 -0300

    feat: add exception handler

---
+++ backend/XgommiApi/src/main/java/br/com/xgommiapi/exception/GommiUserNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.xgommiapi.exception;
+
+public class GommiUserNotFoundException extends ApplicationException{
+    public GommiUserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ backend/XgommiApi/src/main/java/br/com/xgommiapi/infra/RestExceptionHandler.java
@@ -0,0 +1,31 @@
+package br.com.xgommiapi.infra;
+
+import br.com.xgommiapi.exception.GommiUserNotFoundException;
+import br.com.xgommiapi.exception.GommiUserNotUniqueException;
+import br.com.xgommiapi.exception.GommiUserNullAtributeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(GommiUserNotFoundException.class)
+    private ResponseEntity<RestExceptionMessage> GommiUserNotFoundException(GommiUserNotFoundException ex) {
+        RestExceptionMessage exceptionResponse = new RestExceptionMessage(HttpStatus.NOT_FOUND, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(GommiUserNotUniqueException.class)
+    private ResponseEntity<RestExceptionMessage> GommiUserNotUniqueException(GommiUserNotUniqueException ex) {
+        RestExceptionMessage exceptionResponse = new RestExceptionMessage(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(GommiUserNullAtributeException.class)
+    private ResponseEntity<RestExceptionMessage> GommiUserNullAtributeException(GommiUserNullAtributeException ex) {
+        RestExceptionMessage exceptionResponse = new RestExceptionMessage(HttpStatus.BAD_REQUEST, ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
+    }
+}
